7c4e971bb970775ff8dbcae2e4bbf328c689a2d2
93hamza <hamzeh.ajami@gmail.com>
2021-03-12 16:38:47 +0200

    movie

---
+++ app.js
@@ -0,0 +1,14 @@
+var car = prompt("please enter your car type");
+
+if(car == "kia")
+{
+    alert("you entered a kia");
+}
+else if (car == "honda")
+{
+    alert("you entered honda");
+}
+else
+{
+    alert("not a good car");
+}
